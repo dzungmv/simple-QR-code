9c4d97b2e72d8b4555ca1476d3a85aef19e3d44c
Dzung <jungjung@Dzungs-MacBook-Pro.local>
2023-06-02 14:14:41 +0700

    fix reload /

--- src/components/auth/private-route.tsx
+++ src/components/auth/private-route.tsx
@@ -1,17 +1,18 @@
 import { selectUser } from '@/redux/slice/userSlice';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 const PrivateRoute = ({ children }: { children: React.ReactNode }) => {
     const router = useRouter();
+    const pathname = usePathname();
 
     const user = useSelector(selectUser);
 
     const uid = user?.uid;
 
     useEffect(() => {
-        !uid ? router.push('/login') : router.push('/');
+        !uid ? router.push('/login') : router.push(pathname);
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [uid]);
     return <>{children}</>;
